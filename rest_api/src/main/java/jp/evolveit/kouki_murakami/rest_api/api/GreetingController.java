1b10ed82e9e799d741cc0feda376edac959f7f32
Kouki Murakami <m.kouki.0928@gmail.com>
2021-11-18 18:19:37 +0900

    fix RequestBody JSON の name を null不許可に変更

--- rest_api/src/main/java/jp/evolveit/kouki_murakami/rest_api/api/GreetingController.java
+++ rest_api/src/main/java/jp/evolveit/kouki_murakami/rest_api/api/GreetingController.java
@@ -1,5 +1,6 @@
 package jp.evolveit.kouki_murakami.rest_api.api;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
     
     @PostMapping("/api/greeting")
-    public GreetingResponse greeting(@RequestBody GreetingRequest request) {
+    public GreetingResponse greeting(@Validated @RequestBody GreetingRequest request) {
         return new GreetingResponse("Hello" + request.getName() + "!");
     }
 }
--- rest_api/src/main/java/jp/evolveit/kouki_murakami/rest_api/api/GreetingRequest.java
+++ rest_api/src/main/java/jp/evolveit/kouki_murakami/rest_api/api/GreetingRequest.java
@@ -1,9 +1,12 @@
 package jp.evolveit.kouki_murakami.rest_api.api;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class GreetingRequest {
 
+    @NotNull
     private String name;
 }
